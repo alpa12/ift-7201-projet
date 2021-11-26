392a364c5ab14daf757769f0c31fcefde502db31
Maxime Poulin-Bellisle <mpbellisle@gmail.com>
2021-11-26 10:39:12 -0500

    tvar poisson et gamma

--- src/utils/risk_measures.py
+++ src/utils/risk_measures.py
@@ -5,14 +5,14 @@ from scipy.stats import poisson, gamma
 def VaR(kappa, claims, prior=None):
     if prior is None:
         (totals := [np.sum(c) for c in claims]).sort()
-        index = int(np.ceil(len(claims) * (1 - kappa)) - 1)
+        index = int(np.ceil(len(claims) * kappa) - 1)
         return totals[index]
     if prior == "poisson":
         _lambda = estimate_poisson_parameters(claims)
-        return poisson.isf(kappa, _lambda)
+        return poisson.ppf(kappa, _lambda)
     if prior == "gamma":
         alpha, theta = estimate_gamma_parameters(claims)
-        return gamma.isf(kappa, alpha, scale=theta)
+        return gamma.ppf(kappa, alpha, scale=theta)
     if prior == "poisson-gamma":
         return 
 
@@ -22,20 +22,28 @@ def TVaR(kappa, claims, prior=None):
         index = int(np.ceil(len(claims) * (1 - kappa)) - 1)
         return np.mean(totals[index:])
     if prior == "poisson":
+        # Hypothèse de sévérité constante
         _lambda = estimate_poisson_parameters(claims)
-        VaR = poisson.isf(kappa, _lambda)
+        var = poisson.ppf(kappa, _lambda)
         E_x_LE_VaR = 0
         val = 1
-        while (val <= VaR):
-            E_x_LE_VaR += val * poisson.pdf(val, _lambda)
+        while (val <= var):
+            E_x_LE_VaR += val * poisson.pmf(val, _lambda)
             val += 1
         E_x = _lambda
         E_x_GE_VaR = E_x - E_x_LE_VaR
+        tvar = (E_x_GE_VaR + var * (poisson.cdf(var, _lambda) - kappa)) / (1 - kappa)
         severities = [c for claim_list in claims for c in claim_list]
-        avg_severity = np.mean(severities)
-        return TVaR * avg_severity
+        severity = np.mean(severities)
+        return tvar * avg_severity
     if prior == "gamma":
-        return
+        # Hypothèse de fréquence constante
+        alpha, theta = estimate_gamma_parameters(claims)
+        var = gamma.ppf(kappa, alpha, scale=theta)
+        tvar = (1 / (1 - kappa)) * alpha * theta * gamma.sf(var, alpha + 1, scale=theta)
+        n_claims = [len(c) for c in claims]
+        frequency = np.mean(n_claims)
+        return tvar * frequency
     if prior == "poisson-gamma": # E. Marceau, Modelisation et evaluation quantitative des risques en actuariat, p. 86
         return
 
@@ -51,3 +59,7 @@ def estimate_gamma_parameters(claims):
     theta = var_severity / avg_severity
     alpha = avg_severity / theta
     return alpha, theta
+
+claims = [[], [1,2,3], [4,2], [], [1,6], [8], [], [2], [1, 5], [], [5]]
+TVaR(0.9, claims, prior=None)
+TVaR(0.9, claims, prior="poisson")
